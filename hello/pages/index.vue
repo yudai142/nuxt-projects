a3b25a13e90ca849b4061c96467ca0664984d742
yudai142 <yudai.shimoji.42865233@gmail.com>
2021-10-03 19:23:17 +0900

    Initial commit

---
+++ hello/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+      <p>{{message}}</p>
+  </section>
+</template>
+
+  
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
