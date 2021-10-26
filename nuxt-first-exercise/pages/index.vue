0463c9f84f703e5b61a63196d4dacd74601e776a
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-10-26 13:46:58 +0900

    演習：初めてのNuxt.js

---
+++ nuxt-first-exercise/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+      <h1>{{message}}</h1>
+      <hr>
+      <router-link to="/mypage">My Page</router-link>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'Top page'
+    }
+  }
+}
+</script>
