b8e7943cee2c1ab93f911479ea12af61fdf288a8
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-10-29 13:14:39 +0900

    動的なルーティング

---
+++ nuxt-routing/pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <h1>{{ message }}</h1>
+      <p>id: {{ this.$route.params.id }}</p>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data: function(){
+    return{
+      message: "users/_id.vueを表示中"
+    }
+  }
+}
+</script>
