778f9909d4adc205cdba5d31413de097b3d8a80b
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-11-04 11:53:22 +0900

    データを取得して一件表示する

---
+++ nuxt-async/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+      {{users[0].id}}, {{users[0].name}}
+  </section>
+</template>
+
+
+<script>
+const axios = require('axios');
+let url = 'https://jsonplaceholder.typicode.com/users';
+export default {
+  asyncData({ params }) {
+    return axios.get(url)
+      .then((res) => {
+        return { users: res.data }
+      })
+  }
+}
+</script>
