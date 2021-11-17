67ac338bcf86c6f8fca84d1dc6c4201228495918
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-11-17 13:27:39 +0900

    モジュールモードの利用

--- nuxt-vuex/pages/index.vue
+++ nuxt-vuex/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="container">
     <div>
-      <p>{{$store.state.message}}</p>
+      <!-- <p>{{$store.state.message}}</p> -->
+      <p>{{$store.state.hello.message}}</p>
       <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
       <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
-      <button v-on:click="$store.dispatch('updateMessageAction', 'Dispach with payload')">Dispatch</button>
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction', 'Dispach with payload')">Dispatch</button> -->
+      <button v-on:click="$store.dispatch('hello/updateMessageAction', 'Dispach with payload')">Dispatch</button>
     </div>
   </section>
 </template>
--- nuxt-vuex/store/index.js
+++ nuxt-vuex/store/index.js
@@ -1,27 +1,27 @@
-import Vuex from "vuex";
-const createStore = () => {
-  return new Vuex.Store({
-    state: function() {
-      return {
-        message: "hello Vuex!"
-      };
-    },
-    mutations: {
-      updateMessage: function(state, payload) {
-        state.message = payload;
-      }
-    },
-    // actions: {
-    // updateMessageAction(context) {
-    // context.commit("updateMessage", "Commit with payload");
-    // }
-    // }
-    actions: {
-      updateMessageAction(context, payload) {
-        context.commit("updateMessage", payload);
-      }
-    }
-  });
-};
-
-export default createStore;
+// import Vuex from "vuex";
+// const createStore = () => {
+// return new Vuex.Store({
+// state: function() {
+// return {
+// message: "hello Vuex!"
+// };
+// },
+// mutations: {
+// updateMessage: function(state, payload) {
+// state.message = payload;
+// }
+// },
+// actions: {
+// updateMessageAction(context) {
+// context.commit("updateMessage", "Commit with payload");
+// }
+// }
+// actions: {
+// updateMessageAction(context, payload) {
+// context.commit("updateMessage", payload);
+// }
+// }
+// });
+// };
+//
+// export default createStore;
