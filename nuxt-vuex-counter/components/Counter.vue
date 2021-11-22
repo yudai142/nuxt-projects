0c5d2632311b0d336424732d578dc459205b3d79
yudai142 <yudai.shimoji.42865233@gmail.com>
2021-11-22 14:21:56 +0900

    カウンターコンポーネント作成

---
+++ nuxt-vuex-counter/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    counter: {{ $store.state.counter.counter }}
+    <button v-on:click="$store.commit('counter/countUp')">+1</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: 'Counter App'
+    }
+  }
+}
+</script>
