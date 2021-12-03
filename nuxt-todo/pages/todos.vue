117cd00291cc6eb2bc61928d4899a9fd5220a33a
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-12-03 14:07:21 +0900

    TODO 追加機能

--- nuxt-todo/pages/todos.vue
+++ nuxt-todo/pages/todos.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
+    <div class="form">
+      <form v-on:submit.prevent="add">
+        <input v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
 
   </div>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+      name: '',
+      done: false
+    }
+  },
   created: function() {
     this.$store.dispach('todos/init')
+  },
+  methods: {
+    add(){
+      this.$store.dispach('todos/add', this.name)
+      this.name = ''
+    }
   }
 }
 </script>
