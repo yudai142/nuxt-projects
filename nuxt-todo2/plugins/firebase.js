e49a57e57d9e9a36866c0231453e2249911f4045
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-12-06 14:41:09 +0900

    nuxt-todo2(テスト用)作成

---
+++ nuxt-todo2/plugins/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const config = {
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export default firebase;
