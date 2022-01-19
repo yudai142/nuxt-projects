b399f2b545475fa1237f96c0e38e8bf707684156
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-01-19 11:17:21 +0900

    89. 時刻フォーマットの成形

--- nuxt-todo/pages/todos.vue
+++ nuxt-todo/pages/todos.vue
@@ -5,7 +5,7 @@
       <li v-for="todo in todos" :key="todo.id">
         <input type="checkbox" v-bind:checked="todo.done" @change="toggle(todo)">
         <span v-bind:class="{ done: todo.done }">
-          {{ todo.name }} {{ todo.created }}
+          {{ todo.name }} {{ todo.created.toDate() | dateFilter }}
         </span>
         <button v-on:click="remove(todo.id)">X</button>
       </li>
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   data: function(){
     return {
@@ -46,6 +47,11 @@ export default {
   computed: {
     todos() {
       return this.$store.state.todos.todos
+    }
+  },
+  filters: {
+    dateFilter: function(date) {
+      return moment(date).format('YYYY/MM/DD HH:mm:ss')
     }
   }
 }
