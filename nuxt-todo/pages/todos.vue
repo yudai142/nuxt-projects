f011b00e7e70abad952206663483553ad4223d63
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-01-19 11:04:22 +0900

    88. TODO実施状況によってスタイルを変更

--- nuxt-todo/pages/todos.vue
+++ nuxt-todo/pages/todos.vue
@@ -4,7 +4,9 @@
     <ul>
       <li v-for="todo in todos" :key="todo.id">
         <input type="checkbox" v-bind:checked="todo.done" @change="toggle(todo)">
-        {{ todo.name }} {{ todo.created }}
+        <span v-bind:class="{ done: todo.done }">
+          {{ todo.name }} {{ todo.created }}
+        </span>
         <button v-on:click="remove(todo.id)">X</button>
       </li>
     </ul>
@@ -48,3 +50,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  li > span.done{
+    text-decoration: line-through;
+  }
+</style>
